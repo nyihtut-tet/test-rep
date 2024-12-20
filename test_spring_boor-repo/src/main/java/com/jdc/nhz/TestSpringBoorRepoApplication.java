cb2fc6a4c0111587007ea93b5fa88f9ee3822701
John Doe <johndoe@example.com>
2024-12-20 11:30:49 +0630

    init proj

---
+++ test_spring_boor-repo/src/main/java/com/jdc/nhz/TestSpringBoorRepoApplication.java
@@ -0,0 +1,13 @@
+package com.jdc.nhz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringBoorRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringBoorRepoApplication.class, args);
+	}
+
+}
---
+++ test_spring_boor-repo/src/test/java/com/jdc/nhz/TestSpringBoorRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdc.nhz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringBoorRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
